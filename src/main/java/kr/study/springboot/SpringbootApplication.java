c091afa1834fd76cc48b3b21c779094d8421f5e9
sajacaros <sajacall82@naver.com>
2025-11-26 23:42:41 +0900

    :sparkles: Containerless 개발 준비

--- src/main/java/kr/study/springboot/SpringbootApplication.java
+++ src/main/java/kr/study/springboot/SpringbootApplication.java
@@ -1,13 +1,10 @@
 package kr.study.springboot;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class SpringbootApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringbootApplication.class, args);
+        System.out.println("Hello, Spring Boot!");
     }
 
 }
