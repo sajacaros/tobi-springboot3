500257c21ea3c899fc0e38aa94c3a78573522bda
sajacaros <sajacall82@naver.com>
2025-11-27 01:12:46 +0900

    :sparkles: 애노테이션 매핑 정보 사용

--- src/main/java/kr/study/springboot/SpringbootApplication.java
+++ src/main/java/kr/study/springboot/SpringbootApplication.java
@@ -1,22 +1,11 @@
 package kr.study.springboot;
 
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.web.context.support.GenericWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
-
-import java.io.IOException;
 
 public class SpringbootApplication {
 
