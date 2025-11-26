219d2800339cf1f8cb3e71348e19943c611c388e
sajacaros <sajacall82@naver.com>
2025-11-27 00:04:52 +0900

    :sparkles: Hello 컨트롤러 매핑과 바인딩

---
+++ src/main/java/kr/study/springboot/HelloController.java
@@ -0,0 +1,7 @@
+package kr.study.springboot;
+
+public class HelloController {
+    public String hello(String name) {
+        return "Hello " + name;
+    }
+}
--- src/main/java/kr/study/springboot/SpringbootApplication.java
+++ src/main/java/kr/study/springboot/SpringbootApplication.java
@@ -20,6 +20,7 @@ public class SpringbootApplication {
     public static void main(String[] args) {
         ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
         WebServer webServer = serverFactory.getWebServer(servletContext -> {
+            HelloController helloController = new HelloController();
             servletContext.addServlet("frontcontroller", new HttpServlet() {
                 @Override
                 protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -27,11 +28,11 @@ public class SpringbootApplication {
                     if(req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
                         String name = req.getParameter("name");
 
+                        String ret = helloController.hello(name); // handler
+
                         resp.setStatus(HttpStatus.OK.value());
                         resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-                        resp.getWriter().println("Hello " + name);
-                    } else if(req.getRequestURI().equals("/user")) {
-                        // not implemented
+                        resp.getWriter().println(ret);
                     } else {
                         resp.setStatus(HttpStatus.NOT_FOUND.value());
                     }
