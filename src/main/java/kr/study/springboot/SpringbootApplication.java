7f3737d084c52a4dd5302dab99f9c4e486241229
sajacaros <sajacall82@naver.com>
2025-11-26 23:45:28 +0900

    :sparkles: 서블릿 컨테이너 띄우기

--- src/main/java/kr/study/springboot/SpringbootApplication.java
+++ src/main/java/kr/study/springboot/SpringbootApplication.java
@@ -1,10 +1,16 @@
 package kr.study.springboot;
 
 
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+
 public class SpringbootApplication {
 
     public static void main(String[] args) {
-        System.out.println("Hello, Spring Boot!");
+        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+        WebServer webServer = serverFactory.getWebServer();
+        webServer.start();
     }
 
 }
