063cebbb76c88d0cb513816c7c99d2969a974d11
sajacaros <sajacall82@naver.com>
2025-12-07 22:33:30 +0900

    backup main 제거

--- src/main/java/kr/study/springboot/backup/MySpringbootApplication.java
+++ src/main/java/kr/study/springboot/backup/MySpringbootApplication.java
@@ -26,8 +26,8 @@ public class MySpringbootApplication {
         // 스프링을 이용할땐 ApplicationContextAware를 구현하기 보단 생성자를 통해 주입받는것이 깔끔해 보임
     }
 
-    public static void main(String[] args) {
-        MySpringApplication.run(SpringbootApplication.class, args);
-        // SpringApplication.run(MySpringbootApplication.class, args);
-    }
+//    public static void main(String[] args) {
+//        MySpringApplication.run(SpringbootApplication.class, args);
+//        // SpringApplication.run(MySpringbootApplication.class, args);
+//    }
 }
