e5ee7f0d6dbdc3e8901092d6da062bbb800572ff
sajacaros <sajacall82@naver.com>
2025-12-07 15:59:52 +0900

    :sparkles: 메타 애노테이션과 합성 애노테이션

--- src/test/java/kr/study/springboot/HelloServiceTest.java
+++ src/test/java/kr/study/springboot/HelloServiceTest.java
@@ -5,17 +5,34 @@ import kr.study.springboot.hello.HelloService;
 import kr.study.springboot.hello.SimpleHelloService;
 import org.junit.jupiter.api.Test;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@UnitTest
+@interface FastUnitTest {
+}
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.ANNOTATION_TYPE, ElementType.METHOD})
+@Test
+@interface UnitTest {
+}
+
 public class HelloServiceTest {
-    @Test
+    @UnitTest
     void simpleHelloService() {
         HelloService helloService = new SimpleHelloService();
         String ret = helloService.sayHello("dukim");
         assertThat(ret).isEqualTo("Hello dukim");
     }
 
-    @Test
+    @FastUnitTest
     void helloDecorator() {
         HelloService helloService = new HelloDecorator(name -> name);
         String ret = helloService.sayHello("dukim");
