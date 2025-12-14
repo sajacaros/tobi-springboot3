38fce4461ec6ee4b8234ad84f19e7540085d3502
sajacaros <sajacall82@naver.com>
2025-12-14 19:22:18 +0900

    :sparkles: 스타터와 Jetty 서버 구성 추가

--- src/main/java/kr/study/springboot/config/autoconfig/JettyWebServerConfig.java
+++ src/main/java/kr/study/springboot/config/autoconfig/JettyWebServerConfig.java
@@ -1,14 +1,15 @@
 package kr.study.springboot.config.autoconfig;
 
 import kr.study.springboot.config.MyAutoConfiguration;
+import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 
 @MyAutoConfiguration //관례
-public class TomcatWebServerConfig {
-    @Bean("tomcatWebServerConfig")
+public class JettyWebServerConfig {
+    @Bean("jettyWebServerConfig")
     public ServletWebServerFactory servletContainer() {
-        return new TomcatServletWebServerFactory();
+        return new JettyServletWebServerFactory();
     }
 }
