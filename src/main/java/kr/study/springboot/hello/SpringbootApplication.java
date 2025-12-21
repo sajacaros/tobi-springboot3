6b4eca981adefc4fa546f0cb1dcb962dff4f1df1
sajacaros <sajacall82@naver.com>
2025-12-21 18:43:38 +0900

    :sparkles: Environment 추상화와 프로퍼티

--- src/main/java/kr/study/springboot/hello/SpringbootApplication.java
+++ src/main/java/kr/study/springboot/hello/SpringbootApplication.java
@@ -1,10 +1,25 @@
 package kr.study.springboot.hello;
 
 import kr.study.springboot.config.MySpringBootApplication;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
 @MySpringBootApplication
 public class SpringbootApplication {
+
+    @Value("${spring.application.name}")
+    private String appName;
+
+    @Bean
+    ApplicationRunner applicationRunner(Environment environment) {
+        return args -> {
+            System.out.println("Hello, Spring Boot!, " + appName);
+        };
+    }
+
     public static void main(String[] args) {
         // MySpringApplication.run(SpringbootApplication.class, args);
         SpringApplication.run(SpringbootApplication.class, args);
