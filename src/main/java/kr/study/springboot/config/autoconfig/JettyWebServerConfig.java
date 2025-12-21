9c8c4bebb3d16246c12c14fa3ddcfeb4865ee577
sajacaros <sajacall82@naver.com>
2025-12-21 19:12:50 +0900

    :sparkles: @Value와 PropertySourcesPlaceholderConfigurer

--- src/main/java/kr/study/springboot/config/autoconfig/JettyWebServerConfig.java
+++ src/main/java/kr/study/springboot/config/autoconfig/JettyWebServerConfig.java
@@ -2,6 +2,7 @@ package kr.study.springboot.config.autoconfig;
 
 import kr.study.springboot.config.ConditionalMyOnClass;
 import kr.study.springboot.config.MyAutoConfiguration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
@@ -12,11 +13,14 @@ import org.springframework.core.env.Environment;
 @MyAutoConfiguration //관례
 @ConditionalMyOnClass("org.eclipse.jetty.server.Server")
 public class JettyWebServerConfig {
+    @Value("${contextPath}")
+    String contextPath;
+
     @Bean("jettyWebServerConfig")
     @ConditionalOnMissingBean
-    public ServletWebServerFactory servletContainer(Environment env) {
+    public ServletWebServerFactory servletContainer() {
         JettyServletWebServerFactory serverFactory = new JettyServletWebServerFactory();
-        serverFactory.setContextPath(env.getProperty("contextPath"));
+        serverFactory.setContextPath(contextPath);
         return serverFactory;
     }
 }
--- src/main/java/kr/study/springboot/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/kr/study/springboot/config/autoconfig/TomcatWebServerConfig.java
@@ -2,20 +2,24 @@ package kr.study.springboot.config.autoconfig;
 
 import kr.study.springboot.config.ConditionalMyOnClass;
 import kr.study.springboot.config.MyAutoConfiguration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.env.Environment;
 
 @MyAutoConfiguration //관례
 @ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
 public class TomcatWebServerConfig {
+    @Value("${contextPath}")
+    String contextPath;
+
     @Bean("tomcatWebServerConfig")
     @ConditionalOnMissingBean
-    public ServletWebServerFactory servletContainer(Environment env) {
+    public ServletWebServerFactory servletContainer() {
+        System.out.println("contextPath = " + contextPath);
         TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-        serverFactory.setContextPath(env.getProperty("contextPath"));
+        serverFactory.setContextPath(contextPath);
         return serverFactory;
     }
 }
